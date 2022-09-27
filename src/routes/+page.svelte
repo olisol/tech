66a64ef29e869c4c0b66259ed64d25331c12d294
olisol <olivia.solis@pm.me>
2022-09-27 14:54:30 +0200

    demo for dark crystal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,37 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1><p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="bg-secret-owner">
+	<!-- HEADER -->
+	<section
+		class="container mx-auto max-w-screen-xxxl h-80 bg-hero-pattern bg-cover bg-center text-white"
+	>
+		<!-- TOP NAV  -->
+		<nav class="flex justify-between p-5 sm:px-10 bg-transparent text-white">
+			<div class="">Logo</div>
+			<div class="">Menu</div>
+		</nav>
+		<!-- Page Content -->
+		<div class="p-5 sm:px-10 lg:pl-36 my-auto">
+			<div class="flex pb-5">
+				<img src="Ico.png" alt="" class="flex-none w-auto" />
+				<div class="h-16 flex items-center"><h1 class="text-3xl sm:text-4xl uppercase b pl-3.5">Back Up a secret</h1></div>
+			</div>
+
+			<p>
+				<strong
+					>To backup a secret you need to choose recovery partners and get their public keys.</strong
+				>
+			</p>
+			<p>
+				This will split your given secret into a set of encrypted backup pieces linked to your
+				choosen partner and stored on the distributed web.
+			</p>
+		</div>
+	</section>
+
+  <!-- Forms -->
+	<section class="container mx-auto max-w-screen-xxxl h-80 ">
+		<div class="p-5 sm:px-10 flex lg:pl-36">
+				<div class="flex items-center flex-none w-auto text-3xl bg-darker text-white w-16 h-16"><spam class="m-auto">1</spam></div>
+				<h2 class="flex items-center text-2xl sm:text-3xl uppercase pl-3.5">Generate a public key to send to a secret owner</h2>
+		</div>
+	</section>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,30 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    colors: {
+      'white': '#fff',
+      'main': '`#D32B79',
+      'dark': '#821A4E',
+      'darker': '#4F1C35',
+      'secret-owner': '#D7B8C0',
+      'transparent': 'rgba(79, 28, 53, 0.5)',
+    },
+
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      'xxxl': '1700px',
+    },
+
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('bg.png')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      }
+    },
   },
   plugins: [],
 }
